acbbdb925a28833304dc80ca6ea84df58c3841d5
Elphyn <vladkarelov14@gmail.com>
2023-10-23 21:22:00 +0900

    First Program on Go

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Введите значение: ")
+	inputString, _ := reader.ReadString('\n')
+	inputString = strings.TrimSpace(inputString)
+	inputInt, _ := strconv.Atoi(inputString)
+	fmt.Println(inputInt)
+	// _ похоже для каких то ошибок, так как метод возвращает несколько аргументов
+	// надо разобраться как считывать несколько переменных с линии в терминале
 }
