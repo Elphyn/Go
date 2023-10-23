91a0935a3386333213bc6033b6d2da48b7611ede
Elphyn <vladkarelov14@gmail.com>
2023-10-23 23:13:30 +0900

    First Program on Go

--- main.go
+++ main.go
@@ -8,7 +8,30 @@ import (
 	"strings"
 )
 
+func Add(a, b int) int {
+	result := a + b
+	return result
+}
+func Sub(a, b int) int {
+	result := a - b
+	return result
+}
+func Mul(a, b int) int {
+	result := a * b
+	return result
+}
+func Div(a, b int) int {
+	if a == 0 || b == 0 {
+		fmt.Println("Error, division by 0 detected")
+	} else {
+		result := a * b
+		return result
+	}
+	return 0
+}
+
 func userInput() (a, b int, operator string) {
+	//add roman bool to return, and roman input, zero check
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		fmt.Println("Введите значения: ")
@@ -16,6 +39,7 @@ func userInput() (a, b int, operator string) {
 		splitInput := strings.Fields(input)
 		fmt.Println(splitInput)
 
+		//debug here shows that for some reason a, b = 0 here, need fix
 		a, _ := strconv.Atoi(splitInput[0])
 		b, _ := strconv.Atoi(splitInput[2])
 		//if splitInput[1] == "+" {
@@ -26,16 +50,25 @@ func userInput() (a, b int, operator string) {
 			fmt.Println("Error, input values must be in range from (0,10) including 0 and 10)")
 			continue
 		}
+		//here exception triggers only if more than 3 arguments, but also user can type in number, operator, operator and so on, so condition need change
 		if len(splitInput) > 3 {
 			fmt.Println("Error, input values must be two numbers and one operator ()")
 			continue
 		}
 		break
+
 	}
 	return
 }
 
 func main() {
 	a, b, operator := userInput()
+	fmt.Println(a)
+	if operator == "/" {
+		fmt.Printf("Result: %d\n", Div(a, b))
+	}
+	if operator == "+" {
+		fmt.Printf("Result: %d\n", Add(a, b))
+	}
 
 }
