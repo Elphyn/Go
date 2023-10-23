a6b37a834292403093d4ad59a640c800165fa2d4
Elphyn <vladkarelov14@gmail.com>
2023-10-23 21:33:59 +0900

    First Program on Go

--- main.go
+++ main.go
@@ -4,17 +4,24 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
+	"reflect"
 	"strings"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Введите значение: ")
-	inputString, _ := reader.ReadString('\n')
-	inputString = strings.TrimSpace(inputString)
-	inputInt, _ := strconv.Atoi(inputString)
-	fmt.Println(inputInt)
+	//fmt.Println("Введите значение: ")
+	//inputString, _ := reader.ReadString('\n')
+	//inputString = strings.TrimSpace(inputString)
+	//inputInt, _ := strconv.Atoi(inputString)
+	//fmt.Println(inputInt)
 	// _ похоже для каких то ошибок, так как метод возвращает несколько аргументов
 	// надо разобраться как считывать несколько переменных с линии в терминале
+	fmt.Println("Введите значения: ")
+	input, _ := reader.ReadString('\n')
+
+	splitInput := strings.Fields(input)
+	fmt.Println(splitInput)
+	fmt.Printf("Type of splitInput: %s\n", reflect.TypeOf(splitInput))
+	fmt.Printf("Type of splitInput[0]: %s\n", reflect.TypeOf(splitInput[0]))
 }
