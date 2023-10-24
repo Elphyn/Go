2a002e5c8b971f4ba08e91846ad5e344f90cf3b7
Elphyn <vladkarelov14@gmail.com>
2023-10-24 17:23:38 +0900

    Testing roman

--- main.go
+++ main.go
@@ -31,6 +31,31 @@ func Div(a, b int) int {
 	return 0
 }
 
+func getIntFromRoman(roman string) int {
+	reversedRomanNumerals := map[string]int{
+		"I":     1,
+		"II":    2,
+		"III":   3,
+		"IV":    4,
+		"V":     5,
+		"VI":    6,
+		"VII":   7,
+		"VIII":  8,
+		"IX":    9,
+		"X":     10,
+		"XI":    11,
+		"XII":   12,
+		"XIII":  13,
+		"XIV":   14,
+		"XV":    15,
+		"XVI":   16,
+		"XVII":  17,
+		"XVIII": 18,
+	}
+	value, _ := reversedRomanNumerals[roman]
+	return value
+}
+
 func getRoman(number int) string {
 	romanNumerals := map[int]string{
 		1:  "I",
@@ -112,8 +137,11 @@ func main() {
 	//	}
 	//
 	//}
-	test := getRoman(18)
-	fmt.Printf("18 in roman is: %s", test)
+
+	n := 5
+	fmt.Printf("%d in roman is: %s", n, getRoman(n))
+	fmt.Println()
+	fmt.Printf("%s in decemal is: %d", getRoman(n), getIntFromRoman(getRoman(n)))
 
 }
 
